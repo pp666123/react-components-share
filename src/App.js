950d298be9507128b7de1b90e157f4753c26f88b
pp666123 <pp666123@gmail.com>
2022-12-24 23:57:37 +0800

    updata

--- src/App.js
+++ src/App.js
@@ -6,17 +6,17 @@ function App() {
     {
       title: "qrcode.react",
       plugin: "yarn add qrcode.react",
-      // iframeSrc: "https://codesandbox.io/s/heuristic-blackwell-604fo6?file=/src/App.js",
+      iframeSrc: "https://codesandbox.io/s/heuristic-blackwell-604fo6?file=/src/App.js",
     },
     {
       title: "sweet-alert-2",
       plugin: "yarn add sweetalert2-react-content sweetalert2",
-      // iframeSrc: "https://codesandbox.io/s/reverent-dream-ncq4sc?file=/src/App.js",
+      iframeSrc: "https://codesandbox.io/s/reverent-dream-ncq4sc?file=/src/App.js",
     },
     {
       title: "javascript-date",
       plugin: "*getMonth start is 0",
-      // iframeSrc: "https://codesandbox.io/s/epic-glitter-bvbcyj?file=/src/App.js:507-512",
+      iframeSrc: "https://codesandbox.io/s/epic-glitter-bvbcyj?file=/src/App.js:507-512",
     },
     {
       title: "react-hook-form",
@@ -27,7 +27,7 @@ function App() {
           <div>{`*use errors function build your style`}</div>
         </div>
       ),
-      // iframeSrc: "",
+      iframeSrc: false,
       components: <DemoReactHookForm />,
     },
   ];
@@ -45,13 +45,15 @@ function App() {
               <h2 className='mt-0 text-decoration-underline'>{item.title}</h2>
               <h3>{item.plugin}</h3>
               <h3 className='text-decoration-underline mt-3'>demo</h3>{" "}
-              {/* <iframe
-                title={item.title}
-                src={item.iframeSrc}
-                className='codesandbox-iframe'
-                allow='accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking'
-                sandbox='allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts'
-              /> */}
+              {item.iframeSrc && (
+                <iframe
+                  title={item.title}
+                  src={item.iframeSrc}
+                  className='codesandbox-iframe'
+                  allow='accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking'
+                  sandbox='allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts'
+                />
+              )}
               {item.components}
               <hr className='h-80-vw' />
             </div>
